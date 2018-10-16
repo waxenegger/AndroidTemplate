943f96d710207c90d54f599a668359faa2a7995c
Harald Waxenegger <harald.waxenegger@gmail.com>
2018-10-16 20:34:21 +1000

    add SDK build

---
+++ jni/Application.mk
@@ -0,0 +1,11 @@
+
+# Uncomment this if you're using STL in your project
+# See CPLUSPLUS-SUPPORT.html in the NDK documentation for more information
+# APP_STL := stlport_static
+
+APP_ABI := x86_64
+
+# Min runtime API level
+APP_PLATFORM := android-28
+
+APP_STL := c++_static
--- src/main/java/template/helloworld/MainActivity.java
+++ src/main/java/template/helloworld/MainActivity.java
@@ -4,15 +4,6 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
 
-public class MainActivity extends Activity
+public class MainActivity extends org.libsdl.app.SDLActivity
 {
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        TextView label = new TextView(this);
-        label.setText("Hello world!");
-
-        setContentView(label);
-    }
 }
