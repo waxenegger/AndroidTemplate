3e5bc2d9216333f70b15ca32a855a266c12919c7
Harald Waxenegger <h.waxenegger@uq.edu.au>
2018-10-23 11:40:18 +1000

    rm unused imports

--- src/main/java/template/helloworld/MainActivity.java
+++ src/main/java/template/helloworld/MainActivity.java
@@ -1,9 +1,5 @@
 package template.helloworld;
 
-import android.app.Activity;
-import android.os.Bundle;
-import android.widget.TextView;
-
 public class MainActivity extends org.libsdl.app.SDLActivity
 {
 }
